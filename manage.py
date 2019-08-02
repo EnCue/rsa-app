fb13cd8ff869d1d40bae78c261f523ff8ed874b0
Christopher <32796106+EnCue@users.noreply.github.com>
2019-08-02 11:21:10 +0800

    Added encryption and decryption functions to RSA scheme class

--- manage.py
+++ manage.py
@@ -7,6 +7,25 @@ class RSA_system:
         self.n = primes[0] * primes[1]
         self.phi = (self.p - 1) * (self.q - 1)
 
+    def display(self):
+        print('New RSA scheme created\n')
+        print('Primes: ' + str(self.p) + ', ' + str(self.q))
+        print('Modulus: ' + str(self.n))
+        print('Phi(n): ' + str(self.phi))
+        print('Public exponent: ' + str(self.b) + '\nPrivate exponent: ' + str(self.a))
+
+    def Encrypt(self, msg):
+        unreduced_e = msg ** self.b
+        e = unreduced_e % self.n
+
+        return e
+
+    def Decrypt(self, msg):
+        unreduced_d = msg ** self.a
+        d = unreduced_d % self.n
+
+        return d
+    
     def EEA(self, b):
         print("Running EEA")
 
@@ -34,8 +53,8 @@ class RSA_system:
             if((o % m) == 0):
                 reduce = False
 
-        print("GCD: " + str(m))
-        print(t_ii)
+        #print("GCD: " + str(m))
+        #print(t_ii)
         #print(r_vals)
 
         if(m == 1):
@@ -44,6 +63,14 @@ class RSA_system:
             #GENERATE A
 
             a = t_ii
+            
+            if(a < 0):
+                a = a + self.phi
+            
+            #prod = a * b
+            #print(prod % self.phi)
+
+            self.a = a
 
             return True
         else:
@@ -65,7 +92,9 @@ def createSystem():
     
     #print(primes)
     #print(n)
-    
+
+    return cryptosystem
+
 
 
 def getExponents(s):
@@ -105,14 +134,45 @@ def getPrimes():
     return p_list
 
 
+def useScheme(cs):
+    mode = input('Encrypt or decrypt a new integer (E or D)\n')
 
-"""r = input('Select a mode: G - generate, E - encrypt, D - decrypt. \n')
+    if(mode.upper() == "E"):
+        plaintext = input('Enter plaintext integer to be encrypted: ')
+        try:
+            msg = int(plaintext)
+            encryptedmsg = cs.Encrypt(msg)
+
+            print('Encrypted form of message: ' + str(encryptedmsg))
+        except:
+            print('Invalid plaintext entered. Try again.')
+            useScheme(cs)
+    elif(mode.upper() == "D"):
+        e_plaintext = input('Enter encrypted message to be decrypted: ')
+        try:
+            e_msg = int(e_plaintext)
+            msg = cs.Decrypt(e_msg)
+
+            print('Encrypted form of message: ' + str(msg))
+        except:
+            print('Invalid plaintext entered. Try again.')
+            useScheme(cs)
+    
+
+
+
+r = input('Select a mode: G - generate, E - encrypt, D - decrypt. \n')
 
 if(r.upper() == "G"):
     print("Generating new RSA cryptosystem.")
-    createSystem()
-"""
+    cs = createSystem()
 
-cs = RSA_system([5, 7])
-cs.EEA(11)
+    cs.display()
+    print("\n\n")
+
+    useScheme(cs)
+
+
+#cs = RSA_system([7, 13])
+#cs.EEA(11)
 
